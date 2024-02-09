b6bdb9f8ac45178052f112bbfa6a230620f8b0a1
WilliamCubbage <Cubbagwm@mail.uc.edu>
2024-02-09 17:47:06 -0500

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TheaterKiosk
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = in.nextInt();
+
+        if (age >= 21)
+        {
+            System.out.println("You get a wristband.");
+        }
+
+        {
+
+        }
+
+
+
+    }
+}
